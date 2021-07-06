39ecf759c2ee636df17d1fb5ba68d1760f0c2b4d
varidmahdi <varidmahdi@gmail.com>
2021-07-06 13:54:41 +0700

    update project

---
+++ docker-project/laravel/perpus-laravel-master/README.md
@@ -0,0 +1,5 @@
+# docker-laravel-phpfpm-nginx-mysql
+
+### Deploy aplikasi perpustakaan menggunakan docker container
+
+referensi project: https://gilacoding.com/read/project-perpustakaan-dengan-laravel
--- docker-project/nodejs/conn.js
+++ docker-project/nodejs/conn.js
@@ -1,10 +1,16 @@
+require('dotenv').config();
+
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "nodejs_api"
+  // host: "localhost",
+  // user: "root",
+  // password: "",
+  // database: "nodejs_api"
+  host: "process.env.DB_HOST",
+  user: "process.env.DB_USER",
+  password: "process.env.DB_PASS",
+  database: "process.env.DB_NAME"
 });
 
 con.connect(function (err) {
