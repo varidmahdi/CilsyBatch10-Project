dff69e8da60dba65fe93b39b5900b74451101b4b
varidmahdi <varidmahdi@gmail.com>
2021-07-28 16:45:25 +0700

    fix code setup jenkins

--- jenkins/jenkins-setup.sh
+++ jenkins/jenkins-setup.sh
@@ -3,6 +3,8 @@ wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > \
 /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
-sudo apt-get install openjdk-8-jdk
-sudo apt-get install jenkins
+sudo apt-get install jenkins -y
+sudo apt-get install openjdk-11-jdk -y
+#sudo apt-get install openjdk-8-jdk
+sudo systemctl restart jenkins
 sudo systemctl status jenkins
